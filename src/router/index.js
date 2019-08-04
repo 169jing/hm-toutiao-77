edd4ee74d50c1a6645ea40f104cc70d859c49243
169 <169@qq.com>
2019-08-04 23:25:40 +0800

    登录页跳转&首页基础页面显示

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,24 @@
 import VueRouter from 'vue-router'
 import Vue from 'vue'
+// 引入登录页组件
 import Login from '@/views/login'
-
+// 引入home首页组件
+import Home from '@/views/home'
+import Welcome from '@/views/welcome'
+// 使用router插件
 Vue.use(VueRouter)
+// 创建router实例对象
 const router = new VueRouter({
-  routes: [{ path: '/login', name: 'login', component: Login }]
+  routes: [
+    { path: '/login', name: 'login', component: Login },
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+      children: [
+        { path: '/', name: 'welcome', component: Welcome }
+      ]
+    }
+  ]
 })
 export default router
