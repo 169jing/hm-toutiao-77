1a58b1938f3c325784f77406beb77f8629aa2e38
169 <169@qq.com>
2019-08-04 19:18:48 +0800

    登录页效验&跳转去首页

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import VueRouter from 'vue-router'
 import Vue from 'vue'
 import Login from '@/views/login'
+
 Vue.use(VueRouter)
 const router = new VueRouter({
   routes: [{ path: '/login', name: 'login', component: Login }]
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,12 +2,87 @@
   <div class="container">
     <el-card class="box-card">
       <img src="../../assets/images/logo_index.png" alt />
+      <el-form :model="loginForm" :rules="loginRules" ref="loginForm">
+        <el-form-item prop="mobile">
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item prop="code">
+          <el-input
+            v-model="loginForm.code"
+            placeholder="请输入验证码"
+            style="width:236px;margin-right: 10px"
+          ></el-input>
+          <el-button>发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button @click="login" type="primary" style="width: 100% ">登录</el-button>
+        </el-form-item>
+      </el-form>
     </el-card>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    // 自定义效验规则 声明效验函数 在return之前 element-ui提供
+    const checkMobile = (rule, value, callback) => {
+      // 效验手机号
+      if (!/^1[3-9]\d{9}$/.test(value)) {
+        return callback(new Error('手机号格式不对'))
+      }
+      callback()
+    }
+    // 绘制表单依赖的数据
+    return {
+      loginForm: {
+        mobile: '',
+        code: ''
+      },
+      // 表单效验规则数据
+      loginRules: {
+        mobile: [
+          { required: true, message: '请输入正确的手机号', trigger: 'blur' },
+          { validator: checkMobile, trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '请输入正确的验证码', trigger: 'blur' },
+          { len: 6, message: '长度是6位', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    login () {
+      // 对整个表单进行效验  调用form 组件提供的方法 validate
+      this.$refs.loginForm.validate(valid => {
+        if (valid) {
+          // alert('效验success')
+          // 请求登录接口
+          this.$http
+            .post(
+              'http://ttapi.research.itcast.cn/mp/v1_0/authorizations',
+              this.loginForm
+            )
+            // res 响应对象  响应主题
+            .then(res => {
+              console.log(res.data)
+              // 登录成功跳转去首页
+              this.$router.push('/')
+            })
+            // catch请求失败的时候调用的方法
+            .catch(() => {
+              // 错误提示  Element-UI消息提示 提供
+              this.$message.error('手机号或验证码错误')
+            })
+        }
+      })
+    }
+  }
+}
 </script>
 
 <style scoped lang="less">
@@ -20,6 +95,14 @@ export default {}
 .box-card {
   width: 400px;
   height: 350px;
-  margin: 60px auto;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -60%);
+  img {
+    display: block;
+    width: 200px;
+    margin: 0 auto 20px;
+  }
 }
 </style>
